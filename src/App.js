e6ee4d9ab1da0d94e2fc880f2ecc414127baa04b
Dwayne Levene <39275855+dwayneglevene@users.noreply.github.com>
2023-07-20 03:23:23 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
-import {BrowserRouter,Routes,Route, HashRouter} from "react-router-dom";
+import {BrowserRouter,Routes,Route, HashRouter,Switch} from "react-router-dom";
 import './App.css';
 import Home from './pages/Home/Home';
+import LoginPage from './pages/Login/loginPage';
 
 function App() {
   return (
   <HashRouter>
   <Routes>
     <Route exact path='/' element={<Home/>}/>
+    <Route exact path='/loginPage' element={<LoginPage/>}/>
 
   
   </Routes>
+  
   </HashRouter>
   );
 }
---
+++ src/pages/Login/loginPage.js
@@ -0,0 +1,21 @@
+import LoginComponent from "../../components/loginComponent";
+
+function ohShit(){
+    console.log("crap")
+}
+
+
+ohShit()
+function LoginPage(){
+
+
+    return(
+        <div>
+            <h1>Login</h1>
+gtgt7g7yhyhujiuhj
+            <LoginComponent/>
+        </div>
+    )
+}
+
+export default LoginPage;
